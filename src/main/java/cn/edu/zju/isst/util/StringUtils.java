96515c731455bb73800e95542a0e25971c28d381
yanguanwei <yanguanwei@qq.com>
2013-12-18 13:24:35 +0800

    fix the md5 utils

--- src/main/java/cn/edu/zju/isst/util/StringUtils.java
+++ src/main/java/cn/edu/zju/isst/util/StringUtils.java
@@ -3,17 +3,26 @@ package cn.edu.zju.isst.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import org.springframework.util.DigestUtils;
-
 public class StringUtils {
     
-    public static String md5(String str) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(str.getBytes());
-            return DigestUtils.md5DigestAsHex(md.digest());
+    public static String md5(String string) {
+        String s = null;  
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        try {  
+            MessageDigest md = MessageDigest.getInstance("MD5");  
+            md.update(string.getBytes());  
+            byte tmp[] = md.digest();
+            char str[] = new char[16 * 2];
+            int k = 0;
+            for (int i = 0; i < 16; i++) {
+                byte byte0 = tmp[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            s = new String(str);
         } catch (NoSuchAlgorithmException e) {
-        }
-        return str;
+            e.printStackTrace();  
+        }  
+        return s;
     }
 }
